8573b3e6a22d79f5a257e063503f97f4f99d6e38
Devdutt Shenoi <devdutt@outlook.in>
2021-04-19 21:10:03 +0530

    Update dependencies and add lock_based mutability to state

--- src/node.rs
+++ src/node.rs
@@ -2,15 +2,11 @@ use crate::{
     raft_proto::{raft_client::RaftClient, raft_server::RaftServer, EntryRequest},
     state::RaftState,
 };
-use std::{
-    collections::{HashMap, VecDeque},
-    error::Error,
-    sync::Arc,
-};
+use std::{error::Error, sync::Arc};
 use tokio::sync::Mutex;
 use tonic::transport::{Channel, Server};
 
-/// A trait to ensures interfaces necessart in types that can be transformed into byte based messages for 
+/// A trait to ensures interfaces necessart in types that can be transformed into byte based messages for
 /// easy transport over the network, ensuring raft based consensus of cluster state.
 pub trait RaftData {
     fn as_bytes(&self) -> Vec<u8>;
@@ -24,13 +20,12 @@ pub struct RaftNode<T> {
 
 impl<T: RaftData + Sync + Send + 'static> RaftNode<T> {
     /// Starts a raft node, consisting of server and client gRPC stubs.
-    pub async fn start(local_addr: String, mut nodes: Vec<String>, id: u8) -> Result<Self, Box<dyn Error>> {
-        let (schedule, executing, free_nodes, log) = (
-            Arc::new(Mutex::new(VecDeque::new())),
-            Arc::new(Mutex::new(HashMap::new())),
-            Arc::new(Mutex::new(vec![])),
-            Arc::new(Mutex::new(vec![])),
-        );
+    pub async fn start(
+        local_addr: String,
+        mut nodes: Vec<String>,
+        id: u8,
+    ) -> Result<Self, Box<dyn Error>> {
+        let log = Arc::new(Mutex::new(vec![]));
 
         // Keep addr of all nodes but the current one in directory.
         nodes.retain(|x| *x != local_addr);
@@ -42,13 +37,7 @@ impl<T: RaftData + Sync + Send + 'static> RaftNode<T> {
         }
 
         // State that is handed over the the server stub on this node
-        let raft = RaftState::new(
-            id,
-            schedule.clone(),
-            executing.clone(),
-            free_nodes.clone(),
-            log.clone(),
-        );
+        let raft = RaftState::new(id, log.clone());
 
         // Server runs on a background thread and handles calls to the node
         tokio::spawn(async move {
@@ -60,7 +49,7 @@ impl<T: RaftData + Sync + Send + 'static> RaftNode<T> {
         });
 
         Ok(Self {
-            raft: RaftState::new(id, schedule, executing, free_nodes, log),
+            raft: RaftState::new(id, log),
             clients,
         })
     }
@@ -68,26 +57,25 @@ impl<T: RaftData + Sync + Send + 'static> RaftNode<T> {
     pub async fn schedule(&mut self, next: T) {
         let mut res = vec![];
         for client in self.clients.clone().iter_mut() {
-            res.push(client.append_entries(EntryRequest {
-                term: self.raft.current_term + 1,
-                id: self.raft.id as u64,
-                prev_index: self.raft.commit_index,
-                prev_term: self.raft.current_term,
-                entry: next.as_bytes(),
-                commit_index: self.raft.commit_index + 1,
-            }).await);
+            let commit_index = *self.raft.commit_index.lock().await;
+            res.push(
+                client
+                    .append_entries(EntryRequest {
+                        term: self.raft.current_term + 1,
+                        id: self.raft.id as u64,
+                        prev_index: commit_index,
+                        prev_term: self.raft.current_term,
+                        entry: next.as_bytes(),
+                        commit_index: commit_index + 1,
+                    })
+                    .await,
+            );
         }
 
         // TODO: Implement schedule()
     }
 
     pub async fn next(&self) {
-        if let Some(id) = self.raft.free_nodes.lock().await.pop() {
-            if let Some(next) = self.raft.schedule.lock().await.pop_front() {
-                self.raft.executing.lock().await.insert(id, next);
-            }
-        }
-
         // TODO: Implement next()
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,7 @@
 use crate::raft_proto::{
     raft_server::Raft, Byte, EntryReply, EntryRequest, Null, VoteReply, VoteRequest,
 };
-use std::{
-    collections::{HashMap, VecDeque},
-    sync::Arc,
-};
+use std::{cmp::min, sync::Arc};
 use tokio::sync::Mutex;
 use tonic::{Request, Response, Status};
 
@@ -21,33 +18,21 @@ pub enum ServerState {
 /// Datastructure to maintain state of cluster over Raft
 pub struct RaftState<T> {
     pub current_term: u64,
-    pub commit_index: u64,
+    pub commit_index: Arc<Mutex<u64>>,
     pub voted_for: u8,
     state: ServerState,
     pub id: u8,
-    pub schedule: Arc<Mutex<VecDeque<T>>>,
-    pub executing: Arc<Mutex<HashMap<u8, T>>>,
-    pub free_nodes: Arc<Mutex<Vec<u8>>>,
-    pub log: Arc<Mutex<Vec<(u64, u8, T)>>>,
+    pub log: Arc<Mutex<Vec<(u64, T)>>>,
 }
 
 impl<T> RaftState<T> {
-    pub fn new(
-        id: u8,
-        schedule: Arc<Mutex<VecDeque<T>>>,
-        executing: Arc<Mutex<HashMap<u8, T>>>,
-        free_nodes: Arc<Mutex<Vec<u8>>>,
-        log: Arc<Mutex<Vec<(u64, u8, T)>>>,
-    ) -> Self {
+    pub fn new(id: u8, log: Arc<Mutex<Vec<(u64, T)>>>) -> Self {
         Self {
             current_term: 0,
-            commit_index: 0,
+            commit_index: Arc::new(Mutex::new(0)),
             voted_for: id,
             state: ServerState::Follower,
             id,
-            schedule,
-            executing,
-            free_nodes,
             log,
         }
     }
@@ -59,9 +44,22 @@ impl<T: Sync + Send + 'static> Raft for RaftState<T> {
         &self,
         request: tonic::Request<VoteRequest>,
     ) -> Result<tonic::Response<VoteReply>, tonic::Status> {
+        let request = request.into_inner();
+        if request.term < self.current_term {
+            return Ok(Response::new(VoteReply {
+                term: self.current_term,
+                grant: false,
+            }));
+        } else if self.voted_for == self.id {
+            return Ok(Response::new(VoteReply {
+                term: self.current_term,
+                grant: true,
+            }));
+        }
+
         Ok(Response::new(VoteReply {
             term: self.current_term,
-            grant: true,
+            grant: false,
         }))
     }
 
@@ -69,6 +67,26 @@ impl<T: Sync + Send + 'static> Raft for RaftState<T> {
         &self,
         request: tonic::Request<EntryRequest>,
     ) -> Result<tonic::Response<EntryReply>, tonic::Status> {
+        let request = request.into_inner();
+        if request.term < self.current_term {
+            return Ok(Response::new(EntryReply {
+                term: self.current_term,
+                success: false,
+            }));
+        } else if request.prev_index > *self.commit_index.lock().await {
+            return Ok(Response::new(EntryReply {
+                term: self.current_term,
+                success: false,
+            }));
+        } else if request.commit_index > *self.commit_index.lock().await {
+            let last_entry_index = match self.log.lock().await.last() {
+                Some(entry) => entry.0,
+                None => 0,
+            };
+            let mut commit_index = self.commit_index.lock().await;
+            *commit_index = min(request.commit_index, last_entry_index);
+        }
+
         Ok(Response::new(EntryReply {
             term: self.current_term,
             success: true,
