1f46d0bee9e44057ddac11fc0200d5b9939ae56c
Devdutt Shenoi <devdutt@outlook.in>
2021-07-11 15:13:21 +0530

    Write out structure for utilization of async-raft traits

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,18 @@
-use std::{collections::HashMap, sync::Arc};
+use async_raft::{
+    async_trait,
+    raft::{
+        AppendEntriesRequest, AppendEntriesResponse, Entry, InstallSnapshotRequest,
+        InstallSnapshotResponse, MembershipConfig, VoteRequest, VoteResponse,
+    },
+    storage::{CurrentSnapshotData, HardState, InitialState},
+    AppData, AppDataResponse, NodeId, RaftNetwork, RaftStorage,
+};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    io::{Cursor, Error},
+    sync::Arc,
+};
 use tokio::sync::Mutex;
 use tonic::{transport::Channel, Request};
 
@@ -9,8 +23,6 @@ pub mod server;
 
 use client::RaftClientStub;
 use server::RaftServerStub;
-
-pub struct RaftNodeInner {}
 
 pub struct RaftNode {
     pub client: RaftClientStub,
@@ -59,5 +71,118 @@ pub async fn try_add(addr: String) -> RaftClient<Channel> {
             }
             Err(_) => continue,
         }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct RaftRequest {
+    data: Vec<u8>,
+}
+
+impl AppData for RaftRequest {}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct RaftResponse(Option<String>);
+
+impl AppDataResponse for RaftResponse {}
+
+#[async_trait::async_trait]
+impl RaftNetwork<RaftRequest> for RaftNode {
+    async fn append_entries(
+        &self,
+        target: NodeId,
+        rpc: AppendEntriesRequest<RaftRequest>,
+    ) -> anyhow::Result<AppendEntriesResponse> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn install_snapshot(
+        &self,
+        target: NodeId,
+        rpc: InstallSnapshotRequest,
+    ) -> anyhow::Result<InstallSnapshotResponse> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn vote(&self, target: NodeId, rpc: VoteRequest) -> anyhow::Result<VoteResponse> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+}
+
+#[async_trait::async_trait]
+impl RaftStorage<RaftRequest, RaftResponse> for RaftNode {
+    type ShutdownError = Error;
+    type Snapshot = Cursor<Vec<u8>>;
+
+    async fn append_entry_to_log(&self, entry: &Entry<RaftRequest>) -> anyhow::Result<()> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn apply_entry_to_state_machine(
+        &self,
+        index: &u64,
+        data: &RaftRequest,
+    ) -> anyhow::Result<RaftResponse> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn create_snapshot(&self) -> anyhow::Result<(String, Box<Self::Snapshot>)> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn delete_logs_from(&self, start: u64, stop: Option<u64>) -> anyhow::Result<()> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn do_log_compaction(&self) -> anyhow::Result<CurrentSnapshotData<Self::Snapshot>> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn finalize_snapshot_installation(
+        &self,
+        index: u64,
+        term: u64,
+        delete_through: Option<u64>,
+        id: String,
+        snapshot: Box<Self::Snapshot>,
+    ) -> anyhow::Result<()> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn get_current_snapshot(
+        &self,
+    ) -> anyhow::Result<Option<CurrentSnapshotData<Self::Snapshot>>> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn get_initial_state(&self) -> anyhow::Result<InitialState> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn get_log_entries(
+        &self,
+        start: u64,
+        stop: u64,
+    ) -> anyhow::Result<Vec<Entry<RaftRequest>>> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn get_membership_config(&self) -> anyhow::Result<MembershipConfig> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn replicate_to_log(&self, entries: &[Entry<RaftRequest>]) -> anyhow::Result<()> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn replicate_to_state_machine(
+        &self,
+        entries: &[(&u64, &RaftRequest)],
+    ) -> anyhow::Result<()> {
+        Err(anyhow::anyhow!("Nah"))
+    }
+
+    async fn save_hard_state(&self, hs: &HardState) -> anyhow::Result<()> {
+        Err(anyhow::anyhow!("Nah"))
     }
 }
